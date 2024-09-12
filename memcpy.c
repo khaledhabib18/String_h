8da75e8713369f0cad323ae84c4587c264b1b819
Khaled Habib <khaled.habib18@gmail.com>
2024-09-12 17:15:31 +0300

    memcpy

--- memcpy.c
+++ memcpy.c
@@ -10,11 +10,10 @@ int main(void)
     return 0;
 }
 
-void *kh_memcpy(void *dest_str, const void * src_str, size_t n)
+void *kh_memcpy(void *dest_str, const void *src_str, size_t n)
 {
     unsigned char *Temp_dest = dest_str;
     unsigned char *Temp_src = src_str;
-
     if ((NULL == dest_str) || (NULL == src_str))
     {
         printf("memCpy function failed to operate -> NULL pointer\n"); // error message
